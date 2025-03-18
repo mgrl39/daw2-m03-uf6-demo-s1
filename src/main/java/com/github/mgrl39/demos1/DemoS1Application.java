bce56cdaf9e4a30f75fa0ed701f43b17c48d0604
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-18 18:28:45 +0100

    first commit

---
+++ src/main/java/com/github/mgrl39/demos1/DemoS1Application.java
@@ -0,0 +1,13 @@
+package com.github.mgrl39.demos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoS1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoS1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/mgrl39/demos1/DemoS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mgrl39.demos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoS1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
